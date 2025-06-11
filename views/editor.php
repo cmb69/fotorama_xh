70584a04535f0de73a48b5e290a242ad77cfa05b
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-11 17:32:38 +0200

    Indent editor form

--- views/editor.php
+++ views/editor.php
@@ -15,9 +15,9 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
 
 <h1>Fotorama – <?=$this->esc($name)?></h1>
 <form action="<?=$action?>" method="post">
-<input type="hidden" name="fotorama_token" value="<?=$this->esc($token)?>">
-<input type="hidden" name="admin" value="plugin_main">
-<input type="hidden" name="fotorama_gallery" value="<?=$this->esc($name)?>">
-<textarea rows="25" cols="80" class="xh_file_edit" name="fotorama_text"><?=$this->esc($xml)?></textarea>
-<button name="action" value="save"><?=$this->text("label_save")?></button>
+  <input type="hidden" name="fotorama_token" value="<?=$this->esc($token)?>">
+  <input type="hidden" name="admin" value="plugin_main">
+  <input type="hidden" name="fotorama_gallery" value="<?=$this->esc($name)?>">
+  <textarea rows="25" cols="80" class="xh_file_edit" name="fotorama_text"><?=$this->esc($xml)?></textarea>
+  <button name="action" value="save"><?=$this->text("label_save")?></button>
 </form>
