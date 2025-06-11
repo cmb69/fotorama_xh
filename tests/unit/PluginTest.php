7e0b2591cf2e20d64db550f36fb8b71dff9239f1
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-11 14:58:04 +0200

    Test Plugin

---
+++ tests/unit/PluginTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fotorama;
+
+use PHPUnit\Framework\TestCase;
+
+class PluginTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        global $pth, $plugin_tx;
+        $pth = ["folder" => ["plugins" => ""]];
+        $plugin_tx = ["fotorama" => []];
+    }
+
+    public function testMakesCreateGalleryCommand(): void
+    {
+        $this->assertInstanceOf(CreateGalleryCommand::class, Plugin::createGalleryCommand());
+    }
+
+    public function testMakesSaveGalleryCommand(): void
+    {
+        $this->assertInstanceOf(SaveGalleryCommand::class, Plugin::saveGalleryCommand());
+    }
+}
--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,23 +1,10 @@
 <?php
 
-/*
-Copyright 2015-2021 Christoph M. Becker
+require_once "./vendor/autoload.php";
 
-This file is part of Fotorama_XH.
+require_once "../plib/classes/View.php";
 
-Fotorama_XH is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Fotorama_XH is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Fotorama_XH.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-require_once './vendor/autoload.php';
-require_once './classes/GalleryService.php';
+require_once "./classes/CreateGalleryCommand.php";
+require_once "./classes/GalleryService.php";
+require_once "./classes/Plugin.php";
+require_once "./classes/SaveGalleryCommand.php";
