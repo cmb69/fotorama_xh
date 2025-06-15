5a33d8416263d8326331dd3b777398ec16f286da
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-15 19:19:24 +0200

    Add basic ThumbnailService test
    We do not actually test the generated image (since this is tedious),
    but only that it is created, and that it has the desired dimensions.

---
+++ tests/model/ThumbnailServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Fotorama\Model;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+class ThumbnailServiceTest extends TestCase
+{
+    public function setUp(): void
+    {
+        vfsStream::setup("root");
+        mkdir(vfsStream::url("root/cache"), 0777);
+    }
+
+    private function sut(): ThumbnailService
+    {
+        return new ThumbnailService(vfsStream::url("root/cache/"));
+    }
+
+    public function testMakesHorizontalThumbnail(): void
+    {
+        $path = __DIR__ . "/../data/XH2.jpg";
+        $this->sut()->makeThumbnail($path, 64);
+        $path = vfsStream::url("root/cache/" . md5($path) . "_64.jpg");
+        $this->assertFileExists($path);
+        $size = getimagesize($path);
+        $this->assertSame(144, $size[0]);
+        $this->assertSame(64, $size[1]);
+    }
+
+    public function testMakesVerticalThumbnail(): void
+    {
+        $path = __DIR__ . "/../data/XH2_vertical.jpg";
+        $this->sut()->makeThumbnail($path, 64);
+        $path = vfsStream::url("root/cache/" . md5($path) . "_64.jpg");
+        $this->assertFileExists($path);
+        $size = getimagesize($path);
+        $this->assertSame(64, $size[0]);
+        $this->assertSame(144, $size[1]);
+    }
+}
