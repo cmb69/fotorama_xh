dfe715fd5e480955cc0525b5265787c1afb6ad07
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-20 19:30:50 +0200

    Back to dev

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -31,7 +31,7 @@ use Plib\View;
 
 class Plugin
 {
-    public const VERSION = "1.0";
+    public const VERSION = "1.1-dev";
 
     public static function galleryCommand(): GalleryCommand
     {
--- tests/PluginInfoCommandTest.php
+++ tests/PluginInfoCommandTest.php
@@ -27,7 +27,7 @@ class PluginInfoCommandTest extends TestCase
     public function testRendersPluginInfo(): void
     {
         $response = $this->sut()();
-        $this->assertSame("Fotorama 1.0", $response->title());
+        $this->assertSame("Fotorama 1.1-dev", $response->title());
         Approvals::verifyHtml($response->output());
     }
 }
