596d7d522789a614d71ded5abe5e2e5e146aef48
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-27 14:36:31 +0200

    Avoid array function expressions and const in fotorama.js

--- fotorama.js
+++ fotorama.js
@@ -21,14 +21,14 @@
 
 import {assert} from "./assert.js";
 
-document.querySelectorAll("figure.fotorama_gallery").forEach(element => {
+document.querySelectorAll("figure.fotorama_gallery").forEach(function (element) {
     assert(element instanceof HTMLElement);
     init(element);
 });
 
 /** @param {HTMLElement} element */
 function init(element) {
-    const config = JSON.parse(element.dataset.config || "{}");
+    var config = JSON.parse(element.dataset.config || "{}");
     // @ts-expect-error
     if ("jQuery" in window && "fotorama" in jQuery()) {
         // @ts-expect-error
